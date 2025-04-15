d3febb0800fadf87d927bb41406ee72df1fd8a49
Rafael André Hoffmann <rafael.hoffmann@omie.com.br>
2025-04-15 12:18:53 -0300

    Initialize node-list feature in MainActivity

--- app/src/main/java/com/desafio/bipa/MainActivity.kt
+++ app/src/main/java/com/desafio/bipa/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.desafio.bipa.core.design_system.BipaLightningTheme
+import com.desafio.bipa.feature.node_list.ui.view.NodeListRoute
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +20,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             BipaLightningTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                NodeListRoute()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BipaLightningTheme {
-        Greeting("Android")
     }
 }
